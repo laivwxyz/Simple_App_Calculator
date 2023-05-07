70c7201d98530b28859b7db7797e756ed7a0a191
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-07 14:52:05 +0800

    if the user choose addition, this will perform the operation and display the result

--- simple_calculator.py
+++ simple_calculator.py
@@ -21,8 +21,13 @@ def calculator():
             # Ask the user for two numbers
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-            
-# Perform the chosen operation and display the result
+
+            # Perform the chosen operation and display the result
+            # if the user choose addition,
+            if choice == 1:
+                result = num1 + num2
+                print(f"{num1} + {num2} = {result}")
+                
 # Ask the user if they want to try again
 # If the user enters an invalid input, a ValueError is raised.           
 # If the user chooses division and enters a second number of zero, a ZeroDivisionError is raised.     
