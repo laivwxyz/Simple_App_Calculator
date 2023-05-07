1e0708ef1410585ab4bd6ae80f8a84f1e3d8d7d5
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-07 14:42:24 +0800

    Pseudocode of the problem has been added

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,15 @@
 # Dela Cruz, Lailanie E. _ BSCPE 1-4
 # Assignment 5
 # Programming Exercise
+
+# CREATING SIMPLE APP CALCULATOR
+# Ask the user to choose a math operation    
+# Check if the choice is valid
+# Ask the user for two numbers
+# Perform the chosen operation and display the result
+# Ask the user if they want to try again
+# If the user enters an invalid input, a ValueError is raised.           
+# If the user chooses division and enters a second number of zero, a ZeroDivisionError is raised.     
+# If any other error occurs, an Exception is raised.
+
+
