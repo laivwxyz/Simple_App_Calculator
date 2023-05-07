3957227a854300ac2a76164ec001d674218c43ce
laivwxyz <delacruzlailanie2004@gmail.com>
2023-05-07 15:12:39 +0800

    Renaming some variables for better understanding

--- simple_calculator.py
+++ simple_calculator.py
@@ -12,35 +12,35 @@ def calculator():
             print("2. Subtraction")
             print("3. Multiplication")
             print("4. Division")
-            choice = int(input("Enter your choice (1-4): "))
+            choose = int(input("Enter your choice (1-4): "))
 
             # Check if the choice is valid
-            if choice not in [1, 2, 3, 4]:
+            if choose not in [1, 2, 3, 4]:
                 raise ValueError("Invalid choice")
 
             # Ask the user for two numbers
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            num_1 = float(input("Enter first number: "))
+            num_2 = float(input("Enter second number: "))
 
             # Perform the chosen operation and display the result
             # if the user choose addition,
-            if choice == 1:
-                result = num1 + num2
-                print(f"{num1} + {num2} = {result}")
+            if choose == 1:
+                result = num_1 + num_2
+                print(f"{num_1} + {num_2} = {result}")
             # if the user choose subtraction,
-            elif choice == 2:
-                result = num1 - num2
-                print(f"{num1} - {num2} = {result}")
+            elif choose == 2:
+                result = num_1 - num_2
+                print(f"{num_1} - {num_2} = {result}")
             # if the user choose multiplication,
-            elif choice == 3:
-                result = num1 * num2
-                print(f"{num1} * {num2} = {result}")
+            elif choose == 3:
+                result = num_1 * num_2
+                print(f"{num_1} * {num_2} = {result}")
             # if the user choose division,
-            elif choice == 4:
-                if num2 == 0:
+            elif choose == 4:
+                if num_2 == 0:
                     raise ZeroDivisionError("Cannot divide by zero.")
-                result = num1 / num2
-                print(f"{num1} / {num2} = {result}")
+                result = num_1 / num_2
+                print(f"{num_1} / {num_2} = {result}")
 
             # Ask the user if they want to try again
             try_again = input("Do you want to try again? (y/n): ")
@@ -49,22 +49,22 @@ def calculator():
                 break
                           
     # If the user enters an invalid input, a ValueError is raised.  
-    except ValueError as e:
+    except ValueError as error:
         # Log the error
-        print(f"Error: {e}")
+        print(f"Error: {error}")
         print("Please enter a valid choice (1-4)")
         calculator()
         
     # If the user chooses division and enters a second number of zero, a ZeroDivisionError is raised.      
-    except ZeroDivisionError as e:
+    except ZeroDivisionError as error:
         # Log the error
-        print(f"Error: {e}")
+        print(f"Error: {error}")
         print("Please enter a non-zero second number")
         calculator()
 
     # If any other error occurs, an Exception is raised.    
-    except Exception as e:
+    except Exception as error:
         # Log any other errors
-        print(f"Error: {e}")         
+        print(f"Error: {error}")         
    
 calculator()
